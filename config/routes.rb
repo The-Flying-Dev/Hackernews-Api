68725b8edaaab1e8d4d5a6063957316779e96938
Leandran <leandranm@outlook.com>
2021-10-18 18:12:03 +0200

    updated comments partial with lin, created comment controller with view and updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'stories#top'
   get 'stories/:id', to:'stories#show', as: :story
+  get 'comments/:id', to: 'comments#show', as: :comment
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

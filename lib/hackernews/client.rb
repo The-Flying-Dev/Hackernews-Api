c0a4b554b954e5d2a28fa1d0c95be953729027a1
Leandran <leandranm@outlook.com>
2021-10-18 16:23:24 +0200

    added client for accessability so controllers can interact with the API

---
+++ lib/hackernews/client.rb
@@ -0,0 +1,35 @@
+#The client makes it easier for controllers to interact with the API
+module Hackernews  
+    class client
+        def initialize
+          @host = 'community-hacker-news-v1.p.rapidapi.com'
+          @key = '0296bd362fmsh4030ee85ded394ep183f48jsn2a8bfd0299b2'
+        end
+
+        def item(id)
+            get("item/#{id}") #interpolating the id into the URI
+        end
+
+        def topstories
+            get('topstories')
+        end
+
+        private
+
+        #requesting path and coverts it to json
+
+        def get(path)
+            response = Excon.get(
+                'https:// + @host' + '/' + path + '.json?print=pretty',
+                headers: {
+                    'x-rapidapi-host' => @host,
+                    'x-rapidapi-key' => @key,
+                }
+            )
+
+            return false if response.status != 200
+
+            JSON.parse(response.body)
+        end
+    end
+end
